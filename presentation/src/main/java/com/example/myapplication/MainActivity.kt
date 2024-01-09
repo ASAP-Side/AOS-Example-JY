09dcac00a055ae69e3049448715bee7f14a9abb2
jiiiiiyoon <wldbs5897@naver.com>
2024-01-09 17:26:31 +0900

    [Feat] 상단 Pink Spacer 생성

--- presentation/src/main/java/com/example/myapplication/MainActivity.kt
+++ presentation/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,14 +3,23 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+import com.example.myapplication.ui.theme.Pink
+import com.example.myapplication.ui.theme.White
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -30,7 +39,21 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TestFrame() {
-    Column() {
+    Column(
+        modifier = Modifier
+            .background(White)
+            .fillMaxSize()
+            .padding(start = 20.dp, end = 20.dp, top = 35.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Spacer(
+            modifier = Modifier
+                .background(Pink)
+                .fillMaxWidth()
+                .height(103.dp)
+        )
+        Spacer(Modifier.height(26.dp))
+
 
     }
 }
