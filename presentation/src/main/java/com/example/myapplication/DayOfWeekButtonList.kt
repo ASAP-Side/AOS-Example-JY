d55dfde01d62a8465928bdb60d6724c2623b59f0
jiiiiiyoon <wldbs5897@naver.com>
2024-01-11 16:13:46 +0900

    [Refactor] 리뷰 반영 @EvergreenTree97 - enum class 생성 후 버튼 selected 상태 관리 수정

--- presentation/src/main/java/com/example/myapplication/DayOfWeekButtonList.kt
+++ presentation/src/main/java/com/example/myapplication/DayOfWeekButtonList.kt
@@ -5,17 +5,14 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.toMutableStateList
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -29,11 +26,22 @@ import com.example.myapplication.ui.theme.Gray
 import com.example.myapplication.ui.theme.MyApplicationTheme
 import com.example.presentation.R
 
+enum class DayOfWeek(val stringRes: Int) {
+    MONDAY(R.string.monday),
+    TUESDAY(R.string.tuesday),
+    WEDNESDAY(R.string.wednesday),
+    THURSDAY(R.string.thursday),
+    FRIDAY(R.string.friday),
+    SATURDAY(R.string.saturday),
+    SUNDAY(R.string.sunday);
+}
+
 @Composable
 fun DayOfWeekButton(
-    text: Int,
+    text: String,
+    selected: Boolean,
+    onClick: () -> Unit,
 ) {
-    var selected by remember { mutableStateOf(false) }
     val bgColor = if (selected) Blue else Gray
 
     Box(
@@ -42,12 +50,12 @@ fun DayOfWeekButton(
             .height(40.dp)
             .width(40.dp)
             .background(color = bgColor)
-            .clickable { selected = !selected },
+            .clickable { onClick() },
     ) {
         Text(
             modifier = Modifier
                 .align(Alignment.Center),
-            text = stringResource(text),
+            text = text,
             color = Black,
             fontSize = 16.sp,
         )
@@ -56,18 +64,23 @@ fun DayOfWeekButton(
 
 @Composable
 fun DayOfWeekButtonList() {
+
+    val dayOfWeekSelects = remember { DayOfWeek.values().map { false }.toMutableStateList() }
+
     Row(
         Modifier
             .fillMaxWidth()
         horizontalArrangement = Arrangement.SpaceBetween
     ) {
-        DayOfWeekButton(text = R.string.monday)
-        DayOfWeekButton(text = R.string.tuesday)
-        DayOfWeekButton(text = R.string.wednesday)
-        DayOfWeekButton(text = R.string.thursday)
-        DayOfWeekButton(text = R.string.friday)
-        DayOfWeekButton(text = R.string.saturday)
-        DayOfWeekButton(text = R.string.sunday)
+        DayOfWeek.values().forEachIndexed { index, dayOfWeek ->
+            DayOfWeekButton(
+                text = stringResource(dayOfWeek.stringRes),
+                selected = dayOfWeekSelects[index],
+                onClick = {
+                    dayOfWeekSelects[index] = !dayOfWeekSelects[index]
+                }
+            )
+        }
     }
 }
 
